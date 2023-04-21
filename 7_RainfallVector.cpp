95d68d97f58e07857790f6ce97c80fdd8a45a25e
minhanh <92451197+taothao120@users.noreply.github.com>
2023-04-21 22:30:31 +0700

    Add files via upload

---
+++ 7_RainfallVector.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector <double> rainfall(12);
+  for (int i = 0; i < 12; i++)
+  {
+    double rain;
+    cout << "Rainfall for month "<<i+1<<": ";
+    cin >> rain;
+    try { 
+      if ( rain < 0 )
+        throw "0";
+    }
+    catch(int n){ cout << "Error" << n ;}
+    rainfall[i] = rain;
+  }
+  
+  double total=0, highest=0, lowest=0;
+  for (int i = 0; i < 12; i++)
+  {
+      total += rainfall[i];
+      if ( highest < rainfall[i] )
+        highest = rainfall[i];
+      if (lowest > rainfall[i] )
+        lowest = rainfall[i];
+  }
+
+  cout << "Total rainfall: "<< total<<endl;
+  cout << "Average rainfall: "<<total/12<<endl;
+  cout << "highest and lowest:" << highest << lowest ;
+
+  return 0;
+  
+}
